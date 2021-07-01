688de3b5ff5aeb5a89a6c7507e6c427e4f72e17c
justin747 <justin747@gmail.com>
2021-06-30 21:53:15 -0700

    Form Added form, enter key submission, and personalized heading

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [headingText, setheadingtext] = useState("");
+
+  function handleChange(event) {
+    console.log(event.target.value);
+    setName(event.target.value);
+  }
+
+  function handleClick(event) {
+    console.log("Clicked!");
+    setheadingtext(name);
+    event.preventDefault();
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {headingText}</h1>
+      <form onSubmit={handleClick}>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="What's your name?"
+          value={name}
+        />
+        <button onClick={handleClick}>Submit</button>
+      </form>
     </div>
   );
 }
